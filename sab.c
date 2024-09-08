5b535586d5b5aceb526f35c92c1c4229393bbe13
Xerx81 <ryuvraj832@gmail.com>
2024-09-08 11:54:23 +0530

    added error handling

--- sab.c
+++ sab.c
@@ -1,33 +1,45 @@
+/*** includes ***/
+
 #include <ctype.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
+/*** data ***/
+
 struct termios orig_termios;
+
+/*** function prototypes ***/
 
 void enableRawMode();
 void disableRawMode();
+void die(const char *s);
+
+/*** init ***/
 
 int main() {
     enableRawMode();
 
     while (1) {
         char c = '\0';
-        read(STDIN_FILENO, &c, 1);
+        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
         if (iscntrl(c)) {
             printf("%d\r\n", c);
         }
         else {
-            printf("%d ('%c')\r\n", c, c);
+            printf("('%c')\r\n", c);
         }
         if (c == 'q') break;
     }
     return 0;
 }
 
+/*** terminal ***/
+
 void enableRawMode() {
-    tcgetattr(STDIN_FILENO, &orig_termios);
+    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
     atexit(disableRawMode);
 
     struct termios raw = orig_termios;
@@ -38,9 +50,14 @@ void enableRawMode() {
     raw.c_cc[VMIN] = 0;
     raw.c_cc[VTIME] = 1;
 
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcgetattr");
 }
 
 void disableRawMode() {
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) die("tcsetattr");
+}
+
+void die(const char *s) {
+    perror(s);
+    exit(1);
 }
